6e2baed01462bf94d43a323b5bcb951ad4908517
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2017-11-07 12:52:00 +0100

    Text Block

--- app/components/pages/Uniavisen.vue
+++ app/components/pages/Uniavisen.vue
@@ -1,15 +1,18 @@
 <template>
     <main class="client-page sections-container">
         <intro name="uniavisen" url="https://uniavisen.dk/" />
+        <text-block title="The Project" text="Ipsum dolor sit amet, consectetur adipisicing elit. Expedita repellendus voluptatem iure! Hic consequatur accusantium, nemo consequuntur culpa quas incidunt at tenetur repudiandae adipisci debitis consectetur repellendus, minima, voluptas non?" />
     </main>
 </template>
 
 <script>
     import Intro from '../sections/clients/Intro';
+    import TextBlock from '../sections/general/TextBlock';
 
     export default {
         components: {
-            Intro
+            Intro,
+            TextBlock
         }
     }
 </script>
---
+++ app/components/sections/general/TextBlock.vue
@@ -0,0 +1,12 @@
+<template>
+    <section class="text-block">
+        <h2>{{ title }}</h2>
+        <p>{{ text }}</p>
+    </section>
+</template>
+
+<script>
+    export default {
+        props: ['title', 'text']
+    }
+</script>

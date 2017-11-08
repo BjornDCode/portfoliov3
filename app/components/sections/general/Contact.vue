13ec5ec92d02619a34c35265b8dd9b158f34a69c
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2017-11-08 13:58:31 +0100

    Hover effect on square link

--- app/components/sections/general/Contact.vue
+++ app/components/sections/general/Contact.vue
@@ -5,11 +5,23 @@
         </div>
         <div class="links">
             <a href="https://twitter.com/LindholmHansen" class="square-link">
-                <img src="images/twitter.svg" alt="Twitter Logo">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="8425.46 5450.219 31.307 25.544">
+                    <g id="twitter-logo-silhouette" transform="translate(8425.999 5394.559)">
+                        <g id="Group_1" data-name="Group 1" transform="translate(0.001 57.441)">
+                        <path id="Path_15" data-name="Path 15" class="twitter" d="M28.025,60.134a11.48,11.48,0,0,1-3.3.905,5.773,5.773,0,0,0,2.527-3.178,11.59,11.59,0,0,1-3.653,1.4A5.753,5.753,0,0,0,13.8,64.5,16.321,16.321,0,0,1,1.953,58.492a5.755,5.755,0,0,0,1.779,7.674,5.749,5.749,0,0,1-2.6-.721v.072a5.754,5.754,0,0,0,4.611,5.637,5.823,5.823,0,0,1-1.515.2,5.518,5.518,0,0,1-1.082-.107A5.752,5.752,0,0,0,8.512,75.24,11.534,11.534,0,0,1,1.372,77.7,12.217,12.217,0,0,1,0,77.618,16.25,16.25,0,0,0,8.813,80.2c10.575,0,16.356-8.759,16.356-16.356l-.019-.744A11.48,11.48,0,0,0,28.025,60.134Z" transform="translate(-0.001 -57.441)"/>
+                        </g>
+                    </g>
+                </svg>
             </a>
             <a href="#" class="square-link">CV</a>
             <a href="https://www.instagram.com/bjornhansendk/" class="square-link">
-                <img src="images/instagram.svg" alt="Instagram Logo">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="8573 5451 24.5 24.5">
+                    <g id="instagram-logo" transform="translate(8573 5451)">
+                        <path id="Path_12" class="instagram" data-name="Path 12" d="M17.739,0H6.761A6.769,6.769,0,0,0,0,6.761V17.739A6.769,6.769,0,0,0,6.761,24.5H17.739A6.769,6.769,0,0,0,24.5,17.739V6.761A6.769,6.769,0,0,0,17.739,0Zm4.588,17.739a4.593,4.593,0,0,1-4.588,4.587H6.761a4.592,4.592,0,0,1-4.587-4.587V6.761A4.593,4.593,0,0,1,6.761,2.174H17.739a4.593,4.593,0,0,1,4.587,4.587V17.739Z"/>
+                        <path id="Path_13" class="instagram" data-name="Path 13" d="M47.281,40.97a6.313,6.313,0,1,0,6.313,6.313A6.32,6.32,0,0,0,47.281,40.97Zm0,10.452a4.139,4.139,0,1,1,4.139-4.139A4.144,4.144,0,0,1,47.281,51.422Z" transform="translate(-35.031 -35.033)"/>
+                    <path id="Path_14" class="instagram" data-name="Path 14" d="M120.515,28.251a1.593,1.593,0,1,0,1.127.467A1.6,1.6,0,0,0,120.515,28.251Z" transform="translate(-101.687 -24.157)"/>
+                    </g>
+                </svg>
             </a>
         </div>
     </section>

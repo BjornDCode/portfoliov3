f0549f480710dbeb97e00bef9c64e96c672a2e9f
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2017-11-08 14:51:14 +0100

    404 page

---
+++ app/components/pages/Page404.vue
@@ -0,0 +1,12 @@
+<template>
+    <main class="page-404">
+        <div class="title">
+            <h1>404</h1>
+        </div>
+        <h4>
+            <a href="/">
+                <img src="images/arrow.svg" alt="Back Arrow"> Go back to the front page
+            </a>
+        </h4>
+    </main>
+</template>
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,7 @@ import Home from '../components/pages/Home';
 import Uniavisen from '../components/pages/Uniavisen';
 import Knabstrup from '../components/pages/Knabstrup';
 import Eraova from '../components/pages/Eraova';
+import Page404 from '../components/pages/Page404';
 
 
 export default [
@@ -9,4 +10,6 @@ export default [
     { path: '/client/uniavisen', component: Uniavisen },
     { path: '/client/knabstrup', component: Knabstrup },
     { path: '/client/eraova', component: Eraova },
+    { path: '/404', component: Page404 },
+    { path: '*', redirect: '/404' },
 ]
